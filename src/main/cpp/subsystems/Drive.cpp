e033006e8bea50421b5fc119554f6f881d35c847
WuYuanhun <30903332+WuYuanhun@users.noreply.github.com>
2019-01-28 14:25:34 +0800

    create Drive Subsystem

---
+++ src/main/cpp/subsystems/Drive.cpp
@@ -0,0 +1,14 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "subsystems/Drive.h"
+
+Drive::Drive() : Subsystem("ExampleSubsystem") {}
+
+void Drive::InitDefaultCommand() {
+  
+}
---
+++ src/main/include/subsystems/Drive.h
@@ -0,0 +1,18 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <frc/commands/Subsystem.h>
+
+class Drive : public frc::Subsystem {
+ private:
+
+ public:
+  Drive();
+  void InitDefaultCommand() override;
+};
