3f6bac0f1982eb180a40fda18c21f73fc1bf0503
WuYuanhun <30903332+WuYuanhun@users.noreply.github.com>
2019-01-30 11:09:17 +0800

    Revert "Revert "merge all subsystems together  (#6)""
    This reverts commit 3be99b2341011d10415aa721ebcb80286bc41f12.

---
+++ src/main/cpp/subsystems/Rabbit.cpp
@@ -0,0 +1,55 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "subsystems/Rabbit.h"
+
+/* static Rabbit */
+std::shared_ptr<WPI_TalonSRX> Rabbit::ratTal;
+std::shared_ptr<rev::CANSparkMax> Rabbit::rabSpark;
+std::shared_ptr<rev::CANEncoder> Rabbit::rabEncoder;
+std::shared_ptr<frc::Solenoid> Rabbit::rabSole;
+
+/* static Deanck*/
+std::shared_ptr<frc::Solenoid> Rabbit::deLift;
+std::shared_ptr<frc::Solenoid> Rabbit::deDick;
+
+
+Rabbit::Rabbit() : Subsystem("Rabbit") 
+{
+    /* Rabit */
+    ratTal.reset(new WPI_TalonSRX(1));
+    rabSpark.reset(new rev::CANSparkMax(2, rev::CANSparkMaxLowLevel::MotorType::kBrushless));
+    rabEncoder.reset(new rev::CANEncoder(*rabSpark));  
+
+    rabStPos = rabEncoder->GetPosition();
+}
+
+void Rabbit::InitDefaultCommand() {
+  // Set the default command for a subsystem here.
+  // SetDefaultCommand(new MySpecialCommand());
+}
+
+double Rabbit::getPosition()
+{
+  return (rabEncoder->GetPosition() - rabStPos); 
+}
+
+void Rabbit::rabDown()
+{
+  while(getPosition()>0)
+  {
+    rabSpark->Set(0.5);
+  }  
+}
+
+void Rabbit::rabStand()
+{
+  while(getPosition()<0.5)
+  {
+    rabSpark->Set(-0.5);
+  }  
+}
---
+++ src/main/include/subsystems/Rabbit.h
@@ -0,0 +1,43 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <frc/commands/Subsystem.h>
+#include <ctre/Phoenix.h>
+#include <rev/CANSparkMax.h>
+#include <rev/CANEncoder.h>
+#include <rev/CANSparkMaxLowLevel.h>
+#include <frc/WPILib.h>
+
+class Rabbit : public frc::Subsystem {
+ private:
+
+  /*Rabit: */
+  static std::shared_ptr<WPI_TalonSRX> ratTal;
+  
+  static std::shared_ptr<rev::CANSparkMax> rabSpark;
+  static std::shared_ptr<rev::CANEncoder> rabEncoder;
+
+  static std::shared_ptr<frc::Solenoid> rabSole;
+
+  /*Deanck*/
+  static std::shared_ptr<frc::Solenoid> deLift;
+  static std::shared_ptr<frc::Solenoid> deDick;
+
+
+  double rabStPos;
+
+
+ public:
+  Rabbit();
+  void InitDefaultCommand() override;
+  
+  void rabDown();
+  void rabStand(); 
+  double getPosition();
+};
